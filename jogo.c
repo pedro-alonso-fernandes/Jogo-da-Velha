cc8a9d24b991b88ab85f6ad5f36cb7b8cb260a49
Pedro Alonso <pedroalonso2005@hotmail.com>
2025-02-14 08:37:01 -0300

    Marcar no tabuleiro funcionando

--- jogo.c
+++ jogo.c
@@ -4,13 +4,20 @@
 #include <locale.h>
 
 char tab[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-char jogada[9] = {0, 0, 0, 5, 0, 0, 0, 0, 0};
-int indice = -1;
+int indiceUsadas = 0;
+
+typedef struct {
+    int usadas[9];
+    int restantes[9];
+
+}Casa;
+
+Casa casas = { {0}, {1, 2, 3, 4, 5, 6, 7, 8, 9} };
 
 int inicio(){
 	int decisao = -1;
-	
-	printf("-----------Jogo da Velha-------------");
+
+	printf("---------------------Jogo da Velha-----------------------");
 	printf("\n");
 	printf("\n");
 	printf("Escolha:");
@@ -22,14 +29,14 @@ int inicio(){
 	printf("0 - Encerrar o programa");
 	printf("\n");
 	printf("\n");
-	
+
 	printf("Resposta: ");
 	scanf("%d", &decisao);
 	printf("\n");
-	printf("-------------------------------------");
+	printf("---------------------------------------------------------");
 	printf("\n");
 	printf("\n");
-	
+
 	return decisao;
 }
 
@@ -47,69 +54,11 @@ int tabuleiro(){
 	printf("    |     |               |    |    \n");
 	printf("\n");
 
-	printf("Resposta: ");	
+	printf("Resposta: ");
 	scanf("%d", &casa);
 	printf("\n");
 
 	return casa;
-}
-
-void marcarTabuleiro(int casa, char simbolo){
-	switch (casa){
-		case 1:
-		tab[0][0] = simbolo;
-		break;
-		
-		case 2:
-		tab[0][1] = simbolo;
-		break;
-		
-		case 3:
-		tab[0][2] = simbolo;
-		break;
-		
-		case 4:
-		tab[1][0] = simbolo;
-		break;
-		
-		case 5:
-		tab[1][1] = simbolo;
-		break;
-		
-		case 6:
-		tab[1][2] = simbolo;
-		break;
-		
-		case 7:
-		tab[2][0] = simbolo;
-		break;
-		
-		case 8:
-		tab[2][1] = simbolo;
-		break;
-		
-		case 9:
-		tab[2][2] = simbolo;
-		break;
-		
-		default:
-		printf("");
-	
-	}
-
-}
-
-bool verificar(int casa){
-	bool usando = false;
-
-	for(int i = 0; i < 9; i++){
-		if(jogada[i] == casa){
-			usando = true;
-		}
-								
-	}
-
-	return usando;
 }
 
 void mensagemErro(char mensagem[100]){
@@ -125,14 +74,89 @@ void mensagemErro(char mensagem[100]){
 	printf("\n");
 }
 
+void salvarMudancas(int casa){
+
+    casas.usadas[indiceUsadas] = casa;
+    indiceUsadas++;
+    casas.restantes[casa-1] = 0;
+
+}
+
+void marcarTabuleiro(int casa, char simbolo){
+	switch (casa){
+		case 1:
+		tab[0][0] = simbolo;
+        salvarMudancas(casa);
+		break;
+
+		case 2:
+		tab[0][1] = simbolo;
+		salvarMudancas(casa);
+		break;
+
+		case 3:
+		tab[0][2] = simbolo;
+		salvarMudancas(casa);
+		break;
+
+		case 4:
+		tab[1][0] = simbolo;
+		salvarMudancas(casa);
+		break;
+
+		case 5:
+		tab[1][1] = simbolo;
+		salvarMudancas(casa);
+		break;
+
+		case 6:
+		tab[1][2] = simbolo;
+		salvarMudancas(casa);
+		break;
+
+		case 7:
+		tab[2][0] = simbolo;
+		salvarMudancas(casa);
+		break;
+
+		case 8:
+		tab[2][1] = simbolo;
+		salvarMudancas(casa);
+		break;
+
+		case 9:
+		tab[2][2] = simbolo;
+		salvarMudancas(casa);
+		break;
+
+		default:
+		mensagemErro("Erro! Não foi possível marcar o tabuleiro!");
+
+	}
+
+}
+
+bool verificarCasa(int casa){
+	bool usando = false;
+
+	for(int i = 0; i < 9; i++){
+		if(casas.usadas[i] == casa){
+			usando = true;
+		}
+
+	}
+
+	return usando;
+}
+
 int main(){
 	setlocale(LC_ALL, "C.UTF-8");
 	bool repetir = false;
-	
+
 	do{
-	
+
 		int decisao = inicio();
-		
+
 		if(decisao != 1 && decisao != 2 && decisao != 0){
 			printf("Não existe essa opção! Escolha uma opção existente!");
 			printf("\n");
@@ -140,14 +164,14 @@ int main(){
 			repetir = true;
 		}
 		else if(decisao == 1){
-			do{			
+			do{
 				int casa = tabuleiro();
 				if(casa < 1 || casa > 9){
 					mensagemErro("Não existe uma casa com esse número! Digite um número válido!");
 					repetir = true;
 					continue;
 				}
-				bool usando = verificar(casa);
+				bool usando = verificarCasa(casa);
 				if(usando){
 					mensagemErro("Erro! A casa selecionada já está sendo usada!");
 					repetir = true;
@@ -156,17 +180,18 @@ int main(){
 				else{
 					repetir = false;
 					marcarTabuleiro(casa, 'X');
-					
+
+
 				}
 
 			} while(repetir);
 		}
 		else if(decisao == 2){
-		
+
 		}
 		else if(decisao == 0){
 			printf("Encerrando o programa...");
-			printf("\n");		
+			printf("\n");
 			repetir = false;
 		}
 	} while(repetir);
