a419b8d4969318a40dee8c583ce0ef16b0559118
Pedro Alonso <pedroalonso2005@hotmail.com>
2025-02-15 12:08:10 -0300

    Controlando as variaveis por structs

--- jogo.c
+++ jogo.c
@@ -3,16 +3,55 @@
 #include <stdbool.h>
 #include <locale.h>
 
-char tab[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-int indiceUsadas = 0;
 
 typedef struct {
+
+    int vitoriasUs1;
+    int vitoriasUs2;
+    int empates;
+
+}Usuario;
+
+typedef struct {
+
+    int vitorias;
+    int empates;
+    int derrotas;
+
+}CPU;
+
+typedef struct {
+
     int usadas[9];
     int restantes[9];
+    int indiceUsadas;
 
 }Casa;
 
-Casa casas = { {0}, {1, 2, 3, 4, 5, 6, 7, 8, 9} };
+typedef struct {
+
+    char tab[3][3];
+    Casa casas;
+    CPU cpu;
+    Usuario usuario;
+
+}Jogo;
+
+// Inicializando variáveis
+Jogo jogo = {
+    // tab[3][3]
+    {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}},
+
+    // struct casas
+    { {0}, {1, 2, 3, 4, 5, 6, 7, 8, 9}, 0 },
+
+    // struct cpu
+    {0, 0, 0},
+
+    //struct usuario
+    {0, 0, 0}
+};
+
 
 int inicio(){
 	int decisao = -1;
@@ -44,13 +83,13 @@ int tabuleiro(){
 	int casa = -1;
 
 	printf("    |     |               |    |    \n");
-	printf(" %c  |  %c  |  %c         1  | 2  | 3  \n", tab[0][0], tab[0][1], tab[0][2]);
+	printf(" %c  |  %c  |  %c         1  | 2  | 3  \n", jogo.tab[0][0], jogo.tab[0][1], jogo.tab[0][2]);
 	printf("____|_____|____       ____|____|____\n");
 	printf("    |     |               |    |    \n");
-	printf(" %c  |  %c  |  %c         4  | 5  | 6  \n", tab[1][0], tab[1][1], tab[1][2]);
+	printf(" %c  |  %c  |  %c         4  | 5  | 6  \n", jogo.tab[1][0], jogo.tab[1][1], jogo.tab[1][2]);
 	printf("____|_____|____       ____|____|____\n");
 	printf("    |     |               |    |    \n");
-	printf(" %c  |  %c  |  %c         7  | 8  | 9  \n", tab[2][0], tab[2][1], tab[2][2]);
+	printf(" %c  |  %c  |  %c         7  | 8  | 9  \n", jogo.tab[2][0], jogo.tab[2][1], jogo.tab[2][2]);
 	printf("    |     |               |    |    \n");
 	printf("\n");
 
@@ -63,69 +102,69 @@ int tabuleiro(){
 
 void mensagemErro(char mensagem[100]){
 	printf("\n");
-	printf("-------------------------------------");
+	printf("---------------------------------------------------------");
 	printf("\n");
 	printf("\n");
 	printf("%s", mensagem);
 	printf("\n");
 	printf("\n");
-	printf("-------------------------------------");
+	printf("---------------------------------------------------------");
 	printf("\n");
 	printf("\n");
 }
 
 void salvarMudancas(int casa){
 
-    casas.usadas[indiceUsadas] = casa;
-    indiceUsadas++;
-    casas.restantes[casa-1] = 0;
+    jogo.casas.usadas[jogo.casas.indiceUsadas] = casa;
+    jogo.casas.indiceUsadas++;
+    jogo.casas.restantes[casa-1] = 0;
 
 }
 
 void marcarTabuleiro(int casa, char simbolo){
 	switch (casa){
 		case 1:
-		tab[0][0] = simbolo;
+		jogo.tab[0][0] = simbolo;
         salvarMudancas(casa);
 		break;
 
 		case 2:
-		tab[0][1] = simbolo;
+		jogo.tab[0][1] = simbolo;
 		salvarMudancas(casa);
 		break;
 
 		case 3:
-		tab[0][2] = simbolo;
+		jogo.tab[0][2] = simbolo;
 		salvarMudancas(casa);
 		break;
 
 		case 4:
-		tab[1][0] = simbolo;
+		jogo.tab[1][0] = simbolo;
 		salvarMudancas(casa);
 		break;
 
 		case 5:
-		tab[1][1] = simbolo;
+		jogo.tab[1][1] = simbolo;
 		salvarMudancas(casa);
 		break;
 
 		case 6:
-		tab[1][2] = simbolo;
+		jogo.tab[1][2] = simbolo;
 		salvarMudancas(casa);
 		break;
 
 		case 7:
-		tab[2][0] = simbolo;
+		jogo.tab[2][0] = simbolo;
 		salvarMudancas(casa);
 		break;
 
 		case 8:
-		tab[2][1] = simbolo;
+		jogo.tab[2][1] = simbolo;
 		salvarMudancas(casa);
 		break;
 
 		case 9:
-		tab[2][2] = simbolo;
+		jogo.tab[2][2] = simbolo;
 		salvarMudancas(casa);
 		break;
 
@@ -140,13 +179,28 @@ bool verificarCasa(int casa){
 	bool usando = false;
 
 	for(int i = 0; i < 9; i++){
-		if(casas.usadas[i] == casa){
+		if(jogo.casas.usadas[i] == casa){
 			usando = true;
+			break;
 		}
 
 	}
 
 	return usando;
+}
+
+bool verificarVitoria(char simbolo){
+    bool vitoria = false;
+
+//    for(int i = 0; i < 3; i++){
+//        for(int j = 0; j < 3; j++){
+//            if(tab[i][j] == simbolo){
+//
+//            }
+//        }
+//    }
+
+    return vitoria;
 }
 
 int main(){
@@ -164,27 +218,29 @@ int main(){
 			repetir = true;
 		}
 		else if(decisao == 1){
+
+            bool continuar = false;
 			do{
 				int casa = tabuleiro();
 				if(casa < 1 || casa > 9){
 					mensagemErro("Não existe uma casa com esse número! Digite um número válido!");
-					repetir = true;
+					continuar = true;
 					continue;
 				}
 				bool usando = verificarCasa(casa);
 				if(usando){
 					mensagemErro("Erro! A casa selecionada já está sendo usada!");
-					repetir = true;
+					continuar = true;
 					continue;
 				}
 				else{
-					repetir = false;
 					marcarTabuleiro(casa, 'X');
+					continuar = true;
 
 
 				}
 
-			} while(repetir);
+			} while(continuar);
 		}
 		else if(decisao == 2){
 
@@ -192,6 +248,9 @@ int main(){
 		else if(decisao == 0){
 			printf("Encerrando o programa...");
 			printf("\n");
+			printf("\n");
+            printf("---------------------------------------------------------");
+            printf("\n");
 			repetir = false;
 		}
 	} while(repetir);
